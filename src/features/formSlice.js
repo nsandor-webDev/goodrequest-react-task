88d379aea9baf1c4d05ee089ab44e82e7e43fba3
nsandor-webDev <96.sandor.norbert@gmail.com>
2021-06-23 16:29:33 +0200

    step two inputs, next dogShelter choice

--- src/features/formSlice.js
+++ src/features/formSlice.js
@@ -7,20 +7,22 @@ export const twoStepFormSlice = createSlice({
         dogShelter: "",
         amount: 5,
         name: "",
-        //surname: "",
-        //email: ""
+        surname: "",
+        email: "",
+        phone: ""
     },
     // The `reducers` field lets us define reducers and generate associated actions
     reducers: {
         toggleSupportType: state => { state.supportType = !state.supportType},
         chooseShelter: (state, action) => { state.dogShelter = action.payload; },
         setAmount: (state, action) => { state.amount = action.payload },   
-        setName: (state, action) => { state.name = action.payload }
-        //setSurname: (state, action) => { state.surname = action.payload },  
-       // setEmail: (state, action) => { state.email = action.payload }
+        setName: (state, action) => { state.name = action.payload },
+        setSurname: (state, action) => { state.surname = action.payload },  
+        setEmail: (state, action) => { state.email = action.payload },
+        setPhone: (state, action) => { state.phone = action.payload }
     }
   });
 
   export const reducer = twoStepFormSlice.reducer
 
-  export const {toggleSupportType, chooseShelter, setAmount, setName, setSurname, setEmail} = twoStepFormSlice.actions
+  export const {toggleSupportType, chooseShelter, setAmount, setName, setSurname, setEmail, setPhone} = twoStepFormSlice.actions
